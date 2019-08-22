c915cccbb0b488922d12c53f66693a0ebe1529ec
Student-bit <54271399+Student-bit@users.noreply.github.com>
2019-08-22 11:17:50 +0930

    Add files via upload

---
+++ BlinkSOS.ino
@@ -0,0 +1,49 @@
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+// S  
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);    
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+// O
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);           
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);  
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);  
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+// S  
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);    
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+                     
+}
